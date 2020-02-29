b858ff66082344705f240001f05e36925d0f34bb
Emiliano Leite <emiliano.leite@creditas.com.br>
2020-02-28 23:28:09 -0300

    refactor(PodcastList): Move 'key' prop to React.Fragment
    I think React like this better (might be wrong).

--- src/components/dumb/PodcastList.tsx
+++ src/components/dumb/PodcastList.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Podcast from "../../types/Podcast";
 
-
 interface Props {
   podcasts: Podcast[];
   handleSubscription?: (podcast: Podcast) => unknown;
@@ -11,12 +10,13 @@ const PodcastList: React.FC<Props> = ({ podcasts, handleSubscription }) => {
   return (
     <ul>
       {podcasts.map(podcast => (
-        <React.Fragment>
-          <li key={podcast.id}> {podcast.title_original} </li>
-          {/* desta maneira abaixo, o componente fica agnóstico em relação a estar sendo renderizado num contexto público/privado */}
-          {handleSubscription && <button onClick={() => handleSubscription(podcast)}>
-            Subscribe
-          </button>}
+        <React.Fragment key={podcast.id}>
+          <li> {podcast.title_original} </li>
+          {handleSubscription && (
+            <button onClick={() => handleSubscription(podcast)}>
+              Subscribe
+            </button>
+          )}
         </React.Fragment>
       ))}
     </ul>
