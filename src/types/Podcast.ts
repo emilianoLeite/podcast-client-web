01cd0df24d547f7a591d21cdf44c0fec87d116a6
Emiliano Leite <emiliano.leite@creditas.com.br>
2020-02-28 22:26:32 -0300

    chore(lint): Remove whitespaces

--- src/types/Podcast.ts
+++ src/types/Podcast.ts
@@ -1,26 +1,26 @@
 interface Podcast {
-    id: string; 
-    rss: string; 
-    audio: string; 
-    image: string; 
+    id: string;
+    rss: string;
+    audio: string;
+    image: string;
     genre_ids: string[];
-    itunes_id: number; 
-    thumbnail: string; 
-    podcast_id: string; 
-    pub_date_ms: number; 
-    title_original: string; 
-    listennotes_url: string; 
-    audio_length_sec: number; 
-    explicit_content: boolean; 
-    title_highlighted: string; 
-    publisher_original: string; 
-    description_original: string; 
-    publisher_highlighted: string; 
-    podcast_title_original: string; 
-    description_highlighted: string; 
-    podcast_listennotes_url: string; 
-    transcripts_highlighted: unknown[]; 
-    podcast_title_highlighted: string; 
+    itunes_id: number;
+    thumbnail: string;
+    podcast_id: string;
+    pub_date_ms: number;
+    title_original: string;
+    listennotes_url: string;
+    audio_length_sec: number;
+    explicit_content: boolean;
+    title_highlighted: string;
+    publisher_original: string;
+    description_original: string;
+    publisher_highlighted: string;
+    podcast_title_original: string;
+    description_highlighted: string;
+    podcast_listennotes_url: string;
+    transcripts_highlighted: unknown[];
+    podcast_title_highlighted: string;
 }
 
 export default Podcast;
