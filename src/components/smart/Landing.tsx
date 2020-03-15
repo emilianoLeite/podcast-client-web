720b355bfe8610c1457ecd5c893c28fc546f1a75
Emiliano Leite <emiliano.leite@creditas.com.br>
2020-03-15 12:16:02 -0300

    chore(Landing): Add comments that illustrate my line of thought

--- src/components/smart/Landing.tsx
+++ src/components/smart/Landing.tsx
@@ -6,12 +6,9 @@ import Podcast from "../../types/Podcast";
 import { subscribe } from "../../shared/firebase";
 import { PublicContext } from "../../context/Auth";
 
-
 const Landing: React.FC = () => {
   const [podcastsList, setPodcastsList] = React.useState<Podcast[]>([]);
   const { currentUser } = React.useContext(PublicContext);
-  // estou seguindo com a ideia de que apenas as SCREENS tem conhecimento sobre
-  // se estão num contexto público/privado
 
   // estava tentando deixar esta funçao fora do comp pra não ficar criando uma função
   // a cada re-render, mas foda-se por enquanto
@@ -22,13 +19,21 @@ const Landing: React.FC = () => {
       // ou seja, ele sempre vai exibir o botão de subscribe, e, seguindo nesta linha,
       // caso o usuário não esteja logado eu posso:
       // 1. Não fazer nada e "falhar silenciosamente"
-      // 2. Exibir um erro (péssimo. Não faz sentido mostrar o botão só pra logo em seguida
-      // mostrar um erro caso o usuário aperte o botão)
+      // 2. Exibir um erro (péssimo. Não faz sentido mostrar o botão de subscribe
+      // só pra logo em seguida mostrar um erro caso o usuário aperte o botão)
+      //  2.1 Por enquanto adotei esta abordagem
       // 3. Redirecionar pro fluxo do login
       // De qualquer maneiraaa não faz sentido o handleSubscription ser opcional
       //  no PodcastList se for assim.
+      //   3.1 Vou tentar seguir com esta lógica, pra entender como implementa
+      //   3.1.1 Não consegui entender como dá pra abrir o widget do firebase Auth
+      //   imperativamente, o react-firebaseui não dá muito controle sobre o
+      //   widget. Acho que a alternativa será usar o firebase/firebaseui-web (
+      //   versão mais pelada do Firebase UI)
       subscribe(podcast, currentUser.uid);
-
+    } else {
+      // should never hit this path
+      alert("Please log in before subscribing to podcasts");
     }
     // como pegar o currentUser? Acho que vou duplicar as coisas mesmo (neste caso
     // o currentUser pra dentor do PublicContexto)
