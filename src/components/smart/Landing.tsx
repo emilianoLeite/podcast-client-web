80a56cbd5d029c3730299936553524c1a77da481
Emiliano Leite <emiliano.leite@creditas.com.br>
2020-03-15 13:43:55 -0300

    refactor(Landing): Move helper function outside component declaration

--- src/components/smart/Landing.tsx
+++ src/components/smart/Landing.tsx
@@ -4,38 +4,35 @@ import PodcastSearch from "./PodcastSearch";
 import PodcastList from "../dumb/PodcastList";
 import Podcast from "../../types/Podcast";
 import { subscribe } from "../../shared/firebase";
-import { PublicContext } from "../../context/Auth";
+import { PublicContext, Public } from "../../context/Auth";
+
+
+const podcastList = (
+  currentUser: Public["currentUser"],
+  podcastsList: Podcast[]
+): JSX.Element => {
+  if (currentUser) {
+    return (
+      <PodcastList
+        handleSubscription={(podcast) => subscribe(podcast, currentUser.uid)}
+        podcasts={podcastsList}
+      />
+    );
+  } else {
+    return <PodcastList podcasts={podcastsList} />;
+  }
+};
 
 const Landing: React.FC = () => {
   const [podcastsList, setPodcastsList] = React.useState<Podcast[]>([]);
   const { currentUser } = React.useContext(PublicContext);
-
-  const handleSubscription = (podcast: Podcast) => {
-    if (currentUser) {
-      subscribe(podcast, currentUser.uid);
-    } else {
-      // should never hit this path
-      alert("Please log in before subscribing to podcasts");
-    }
-  };
-
-  const podcastList = (): JSX.Element => {
-    return currentUser ? (
-      <PodcastList
-        handleSubscription={handleSubscription}
-        podcasts={podcastsList}
-      />
-    ) : (
-      <PodcastList podcasts={podcastsList} />
-    );
-  };
 
   return (
     <div>
       <h1>Home</h1>
       <PodcastSearch handleSearchResults={setPodcastsList} />
 
-      {podcastList()}
+      {podcastList(currentUser, podcastsList)}
     </div>
   );
 };
