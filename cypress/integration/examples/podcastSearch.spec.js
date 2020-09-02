8f008ecf57f62fc0cdfe50f2a8ec62e29b8c8c47
Emiliano Leite <emiliano.leite@creditas.com.br>
2020-09-01 23:54:45 -0300

    test(PodcastSearch): Create e2e test

---
+++ cypress/integration/examples/podcastSearch.spec.js
@@ -0,0 +1,26 @@
+describe('When searching for podcasts', () => {
+  describe('when some results are returnted', () => {
+    beforeEach(() => {
+      cy.server()
+      cy.fixture('podcastSearchResult.json').as('mockPodcastSearchResults')
+      cy.route('GET', 'https://listen-api.listennotes.com/api/v2/search?type=podcast&q=*', '@mockPodcastSearchResults').as('podcastSearchResults')
+    })
+
+    it('correctly renders the returned podcasts in a list', () => {
+      cy.visit('http://localhost:3000')
+
+      cy.get('[type="text"]')
+        .type('Star Wars')
+
+      cy.contains('Search').click()
+
+      cy.get('[data-testid^="list-item"]')
+        .should('have.length', 10)
+        .then(async (listItems) =>
+          listItems.each((_index, listItem) =>
+            cy.wrap(listItem).within(() => cy.contains('star wars', { matchCase: false }))
+          )
+        )
+    })
+  })
+})
