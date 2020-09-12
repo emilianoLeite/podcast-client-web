f04de34798eddcb0844a974652731052e0aed7b5
Emiliano Leite <emiliano.leite@creditas.com.br>
2020-09-12 00:13:18 -0300

    feat(Navbar): Conditionally render public/private links

--- src/components/dumb/Navbar.tsx
+++ src/components/dumb/Navbar.tsx
@@ -1,14 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
 
 import FirebaseAuth from "../smart/components/FirebaseAuth";
 import { Link } from "react-router-dom";
+import { PublicContext } from "../../context/Auth";
+
+const privateLinks = () => <React.Fragment>
+  | <Link to="/home">Dashboard</Link>{" "}
+  | <Link to="/play_queue">Play Queue</Link>{" "}
+</React.Fragment>;
 
 const Navbar: React.FC = () => {
+  const { currentUser } = useContext(PublicContext);
+
   return (
     <nav>
       <Link to="/">Home</Link>{" "}
-      | <Link to="/home">Dashboard</Link>{" "}
-      | <FirebaseAuth />
+      {currentUser ? privateLinks() : <FirebaseAuth />}
     </nav>
   );
 };
